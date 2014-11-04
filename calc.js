6030aff3c0cbbe1187f06240b5501c7b7744b466
Deannaludwig <Deanna.Ludwig1@marist.edu>
2014-11-04 16:01:01 -0500

    finished the JavaScript part

--- calc.js
+++ calc.js
@@ -14,11 +14,41 @@ var readNumbers = function(){
   return result[0];
 };
 
+var fib = function(number)
+{
+	var result;
+	var x = 0;
+	var y = 1;
+	
+	for(var i = 0; i <= number; i++)
+	{
+		result = x;
+		x = y;
+		y = result + x;
+	}
+	
+	return result;
+};
 
+var tri = function(n)
+{
+	var result = 0;
+	
+	for(var i = 0; i < n; i++)
+	{
+		result = (n*(n + 1))/2;
+	}
+	
+	return result;
+};
 var run = function() {
 	
+	var number = readNumbers();
+	var trin = tri(number);
+	var fibo = fib(number);
+	
 	var output = document.getElementById('result');
-	output.innerHTML = 'the average score is ' + avg;
+	output.innerHTML = 'The ' + number + 'th number in the fibanacci sequence is ' + fibo + '. In the triangular sequence the ' + number + 'th is ' + trin + '.';
 };
 
 var elem = document.getElementById('runner');
